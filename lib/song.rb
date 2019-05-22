2a8c666c0dbc6ed970332c80befd39f30ea52865
Caleb Aviles <earthctzn1@gmail.com>
2019-05-22 02:29:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,9 @@ class Song
     artist_name = filename.split(" - ")[0]
     song_name = filename.split(" - ")[1]
     song = Song.new(song_name)
-    song.artist = Artist.find_or_create_by_name(artist_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    song.artist = artist
+    artist.add_song
     return song 
   end
 
