146db71612f3e08e76aa54bee575c250162f9e4c
Caleb Aviles <earthctzn1@gmail.com>
2019-05-22 01:54:10 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,6 +4,9 @@ class MP3Importer
     @path = path
   end
   
+  def files
+  end
+  
   def import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
   end
