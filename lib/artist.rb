1f6ae180657328a38b196eb487691a6d4a90c27a
Caleb Aviles <earthctzn1@gmail.com>
2019-05-22 00:59:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,7 +31,7 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-     self.all.detect{|x| x.name == name}
+     match = self.all.detect{|x| x.name == name}
     if
       #binding.pry
       
