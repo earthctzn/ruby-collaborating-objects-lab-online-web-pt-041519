a829350707a4175ffd4b9c1d665a2bf3f51f0c75
Caleb Aviles <earthctzn1@gmail.com>
2019-05-22 03:09:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,11 @@
 class Song 
-  @@all = []
+
   attr_accessor :name, :artist 
   
   def initialize(name)
     @name = name
-    @@all << self
   end
   
-  def self.all
-    @@all
-  end
   
   def self.new_by_filename(filename)
     artist_name = filename.split(" - ")[0]
