b183dec326d27bfc7f3ca96e1a055de71d44402f
Caleb Aviles <earthctzn1@gmail.com>
2019-05-22 02:18:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
     artist_name = filename.split(" - ")[0]
     song_name = filename.split(" - ")[1]
     song = Song.new(song_name)
-
+    song.artist = Artist.find_or_create_by_name(artist_name)
   end
 
 end
